2d64a9488b0b936f3861f2f3f3c6e3dc51712e5a
Samir <122364498+Stileexx@users.noreply.github.com>
2024-04-03 23:25:33 +0200

    == checks the reference (this case true)

--- src/Casing.java
+++ src/Casing.java
@@ -1,7 +1,14 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Casing {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+
+        String a = "Samir";
+        String b = "Samir";
+
+        System.out.println(a == b);
     }
 }
