4c2f9138ae21c8bc7d655923320c9f19a48c773a
Samir <122364498+Stileexx@users.noreply.github.com>
2024-04-05 14:50:20 +0200

    switch casing

--- src/Casing.java
+++ src/Casing.java
@@ -5,10 +5,23 @@ public class Casing {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        String fruit = input.next();
 
-        String a = "Samir";
-        String b = "Samir";
-
-        System.out.println(a == b);
+        switch (fruit) {
+            case "Mango":
+                System.out.println("King of fruits");
+                break;
+            case "Apple":
+                System.out.println("A sweat red fruit");
+                break;
+            case "Banana":
+                System.out.println("A long yellow fruit");
+                break;
+            case "Orange":
+                System.out.println("A round fruit");
+                break;
+            default:
+                System.out.println("Please valid a valid fruit");
+        }
     }
 }
