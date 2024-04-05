725ae54ef6f964823cc1b15a78c49f71b8d8c7f2
Samir <122364498+Stileexx@users.noreply.github.com>
2024-04-05 14:55:44 +0200

    ENHANCED switch statement

--- src/Casing.java
+++ src/Casing.java
@@ -1,27 +1,52 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Casing {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        String fruit = input.next();
+//        String fruit = input.next();
+//
+//        switch (fruit) {
+//            case "Mango" -> System.out.println("King of fruits");
+//            case "Apple" -> System.out.println("A sweat red fruit");
+//            case "Banana" -> System.out.println("A long yellow fruit");
+//            case "Orange" -> System.out.println("A round fruit");
+//            default -> System.out.println("Please valid a valid fruit");
+//        }
 
-        switch (fruit) {
-            case "Mango":
-                System.out.println("King of fruits");
+        int day = input.nextInt();
+
+//        switch (day) {
+//            case 1 -> System.out.println("Monday");
+//            case 2 -> System.out.println("Tuesday");
+//            case 3 -> System.out.println("Wednesday");
+//            case 4 -> System.out.println("Thurstday");
+//            case 5 -> System.out.println("Friday");
+//            case 6 -> System.out.println("Satarday");
+//            case 7 -> System.out.println("Sunday");
+//
+//        }
+
+
+//        Enhanced Switch Statement
+        switch (day) {
+            case 1,2,3,4,5 -> System.out.println("Weakday");
+            case 6,7 -> System.out.println("Weekend");
+        }
+
+//        Normal Switch Statement
+        switch (day) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Weekday");
                 break;
-            case "Apple":
-                System.out.println("A sweat red fruit");
+            case 6:
+            case 7:
+                System.out.println("Weekend");
                 break;
-            case "Banana":
-                System.out.println("A long yellow fruit");
-                break;
-            case "Orange":
-                System.out.println("A round fruit");
-                break;
-            default:
-                System.out.println("Please valid a valid fruit");
         }
     }
 }
