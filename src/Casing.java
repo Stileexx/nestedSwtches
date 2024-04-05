fd2816529e87fbc71d323c99dae51a4b672a6887
Samir <122364498+Stileexx@users.noreply.github.com>
2024-04-05 15:23:47 +0200

    Complete File (final benefit of not using break)

--- src/Casing.java
+++ src/Casing.java
@@ -13,17 +13,39 @@ public class Casing {
 //            case "Orange" -> System.out.println("A round fruit");
 //            default -> System.out.println("Please valid a valid fruit");
 //        }
-
+//
         int day = input.nextInt();
+//
+//        switch (day) {
+//            case 1 -> System.out.println("Monday");
+//            case 2 -> System.out.println("Tuesday");
+//            case 3 -> System.out.println("Wednesday");
+//            case 4 -> System.out.println("Thursday");
+//            case 5 -> System.out.println("Friday");
+//            case 6 -> System.out.println("Saturday");
+//            case 7 -> System.out.println("Sunday");
+//
+//        }
 
+
+//        Enhanced Switch Statement
         switch (day) {
-            case 1 -> System.out.println("Monday");
-            case 2 -> System.out.println("Tuesday");
-            case 3 -> System.out.println("Wednesday");
-            case 4 -> System.out.println("Thursday");
-            case 5 -> System.out.println("Friday");
-            case 6 -> System.out.println("Saturday");
-            case 7 -> System.out.println("Sunday");
+            case 1,2,3,4,5 -> System.out.println("Weekday");
+            case 6,7 -> System.out.println("Weekend");
+        }
+
+        //Normal Switch Statement
+        switch (day) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Weekday");
+                break;
+            case 6:
+            case 7:
+                System.out.println("Weekend");
 
         }
     }
