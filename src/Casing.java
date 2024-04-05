5e4042fd3b5d537e9ac3ebfc5b10d4be5d1fe435
Samir <122364498+Stileexx@users.noreply.github.com>
2024-04-05 15:12:37 +0200

    Benefit of not using break;

--- src/Casing.java
+++ src/Casing.java
@@ -20,9 +20,9 @@ public class Casing {
 //            case 1 -> System.out.println("Monday");
 //            case 2 -> System.out.println("Tuesday");
 //            case 3 -> System.out.println("Wednesday");
-//            case 4 -> System.out.println("Thurstday");
+//            case 4 -> System.out.println("Thursday");
 //            case 5 -> System.out.println("Friday");
-//            case 6 -> System.out.println("Satarday");
+//            case 6 -> System.out.println("Saturday");
 //            case 7 -> System.out.println("Sunday");
 //
 //        }
@@ -30,7 +30,7 @@ public class Casing {
 
 //        Enhanced Switch Statement
         switch (day) {
-            case 1,2,3,4,5 -> System.out.println("Weakday");
+            case 1,2,3,4,5 -> System.out.println("Weekday");
             case 6,7 -> System.out.println("Weekend");
         }
 
