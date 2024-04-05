71a5016f332b02866446609b6196a6edfb9d352f
Samir <122364498+Stileexx@users.noreply.github.com>
2024-04-05 14:55:44 +0200

    ENHANCED switch statement

--- src/Casing.java
+++ src/Casing.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Casing {
@@ -8,20 +7,11 @@ public class Casing {
         String fruit = input.next();
 
         switch (fruit) {
-            case "Mango":
-                System.out.println("King of fruits");
-                break;
-            case "Apple":
-                System.out.println("A sweat red fruit");
-                break;
-            case "Banana":
-                System.out.println("A long yellow fruit");
-                break;
-            case "Orange":
-                System.out.println("A round fruit");
-                break;
-            default:
-                System.out.println("Please valid a valid fruit");
+            case "Mango" -> System.out.println("King of fruits");
+            case "Apple" -> System.out.println("A sweat red fruit");
+            case "Banana" -> System.out.println("A long yellow fruit");
+            case "Orange" -> System.out.println("A round fruit");
+            default -> System.out.println("Please valid a valid fruit");
         }
     }
 }
